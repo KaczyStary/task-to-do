5cb5ebae4ae961aa3f8078769af21c03e60c1021
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-04-24 11:03:06 +0200

    delete annotation dev profile

--- src/main/java/com/example/tasktodo/config/TableConfiguration.java
+++ src/main/java/com/example/tasktodo/config/TableConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Data
-@Profile("dev")
 public class TableConfiguration implements ITableConfiguration{
 
     @Value("${table-color}")
