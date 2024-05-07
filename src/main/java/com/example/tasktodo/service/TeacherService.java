b388909b573fb6ef3f6f3b734d69b100d9a7cbe8
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-07 17:49:24 +0200

    add methods

--- src/main/java/com/example/tasktodo/service/TeacherService.java
+++ src/main/java/com/example/tasktodo/service/TeacherService.java
@@ -1,14 +1,27 @@
 package com.example.tasktodo.service;
 
 import com.example.tasktodo.entity.Student;
+import com.example.tasktodo.entity.StudentTask;
+import com.example.tasktodo.entity.Task;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 @Service
 public interface TeacherService {
 
     List<Student> findAllStudentsByTeacherId(long id);
+    Map<Long, List<StudentTask>> findAllTaskForStudentByTeacherId(long id);
     Student findStudentById(long id);
+    Task findTaskById(long id);
+    StudentTask findStudentTaskByStudentIdAndTaskId(long studentId, long taskId);
+    List<Task> findAllTaskByTeacherId(long teacherId);
+    @Transactional
+    void rateStudentTask(StudentTask task);
+    @Transactional
+    void deleteRateStudentTask(StudentTask studentTask);
+    @Transactional
+    void updateTask(Task task);
 }
