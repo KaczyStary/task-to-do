aea5730f012492003cc92e93b086ec41a4521069
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-07 17:55:55 +0200

    add mappings

--- src/main/java/com/example/tasktodo/controller/StudentController.java
+++ src/main/java/com/example/tasktodo/controller/StudentController.java
@@ -1,7 +1,7 @@
 package com.example.tasktodo.controller;
 
+import com.example.tasktodo.config.IAppConfiguration;
 import com.example.tasktodo.config.ITableConfiguration;
-import com.example.tasktodo.config.TableConfiguration;
 import com.example.tasktodo.entity.Student;
 import com.example.tasktodo.entity.StudentTask;
 import com.example.tasktodo.service.StudentService;
@@ -17,29 +17,36 @@ public class StudentController {
 
     private final StudentService studentService;
     private final ITableConfiguration tableConfiguration;
+    private final IAppConfiguration appConfiguration;
 
-    public StudentController(StudentService studentService, ITableConfiguration tableConfiguration) {
+    public StudentController(StudentService studentService, ITableConfiguration tableConfiguration, IAppConfiguration appConfiguration) {
         this.studentService = studentService;
         this.tableConfiguration = tableConfiguration;
+        this.appConfiguration = appConfiguration;
     }
 
     @GetMapping("/student")
     public String studentTaskList(@AuthenticationPrincipal Student student, Model model) {
         model.addAttribute("tableColor", tableConfiguration.getTableColor() + " table");
         model.addAttribute("tasks", studentService.findAllTaskForStudent(student.getId()));
+        model.addAttribute("pageTheme", appConfiguration.getPageTheme());
 
         return "./student/tasks";
     }
 
     @GetMapping("/student/home")
     public String homeStudent(@AuthenticationPrincipal Student student, Model model){
-        model.addAttribute("student", student);
+        model.addAttribute("tasks", studentService.findAllTaskForStudent(student.getId()));
+        model.addAttribute("pageTheme", appConfiguration.getPageTheme());
+
         return "./student/home-student";
     }
 
     @GetMapping("/student/task/{id}")
     public String showTaskForm(@PathVariable long id, Model model){
         final Optional<StudentTask> studentTaskById = studentService.findStudentTaskById(id);
+        model.addAttribute("pageTheme", appConfiguration.getPageTheme());
+
         if(studentTaskById.isPresent()) {
             model.addAttribute("task", studentTaskById.get());
             return "./student/task-form";
@@ -49,9 +56,39 @@ public class StudentController {
         }
     }
 
+    @GetMapping("/student/task-edit/{id}")
+    public String showTaskEdit(@PathVariable long id, Model model){
+        final Optional<StudentTask> studentTaskById = studentService.findStudentTaskById(id);
+        model.addAttribute("pageTheme", appConfiguration.getPageTheme());
+
+        if(studentTaskById.isPresent()) {
+            model.addAttribute("task", studentTaskById.get());
+            return "./student/task-form";
+        }else {
+            model.addAttribute("message", "Task not found");
+            return "error";
+        }
+    }
+
+    @GetMapping("/student/task-delete/{id}")
+    public String deleteStudentTask(@PathVariable long id, @AuthenticationPrincipal Student student, Model model){
+        Optional<StudentTask> opTask = studentService.findStudentTaskById(id);
+        model.addAttribute("pageTheme", appConfiguration.getPageTheme());
+
+        if(opTask.isPresent()) {
+            studentService.deleteStudentTask(student.getId(), opTask.get());
+            return "redirect:/student/home";
+        } else {
+            model.addAttribute("message", "StudentTask not found");
+            return "error";
+        }
+    }
+
     @GetMapping("/student/task-description/{id}")
     public String showTaskDescription(@PathVariable long id, Model model){
         final Optional<StudentTask> studentTaskById = studentService.findStudentTaskById(id);
+        model.addAttribute("pageTheme", appConfiguration.getPageTheme());
+
         if(studentTaskById.isPresent()) {
             model.addAttribute("taskDescription", studentTaskById.get().getTask());
             return "./student/task-description";
@@ -64,6 +101,7 @@ public class StudentController {
     @PostMapping("/student/finish-task")
     public String finishStudentTask(@ModelAttribute StudentTask studentTask){
         studentService.finishStudentTask(0, studentTask);
-        return "redirect:/student";
+        return "redirect:/student/home";
     }
+
 }
