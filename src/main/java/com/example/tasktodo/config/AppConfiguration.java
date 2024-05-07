56e857a6e1e10df857a6634b75f7fb812fbd76e7
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-07 17:54:28 +0200

    add variable

--- src/main/java/com/example/tasktodo/config/AppConfiguration.java
+++ src/main/java/com/example/tasktodo/config/AppConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.tasktodo.config;
 
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,6 +16,9 @@ public class AppConfiguration implements IAppConfiguration{
 
     @Value("${page.start}")
     private int pageStart;
+
+    @Value("${page.theme}")
+    private String pageTheme;
 
 //    @Value("${password}")
 //    private String password;
