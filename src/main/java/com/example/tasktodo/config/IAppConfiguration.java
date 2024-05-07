b02722eaa8f70f2b4084d002e88edf00978d731d
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-07 17:55:00 +0200

    add method

--- src/main/java/com/example/tasktodo/config/IAppConfiguration.java
+++ src/main/java/com/example/tasktodo/config/IAppConfiguration.java
@@ -3,5 +3,6 @@ package com.example.tasktodo.config;
 public interface IAppConfiguration {
     int getPageSize();
     int getPageStart();
+    String getPageTheme();
     //String getPassword();
 }
